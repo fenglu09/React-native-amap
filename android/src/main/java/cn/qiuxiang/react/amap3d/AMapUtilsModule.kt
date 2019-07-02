b252870a592d2cb47f4a8f4096cdfe5e8f11b628
how <370969878@qq.com>
2019-07-02 10:55:26 +0800

    gps坐标转换成高德坐标

--- android/src/main/java/cn/qiuxiang/react/amap3d/AMapUtilsModule.kt
+++ android/src/main/java/cn/qiuxiang/react/amap3d/AMapUtilsModule.kt
@@ -2,10 +2,7 @@ package cn.qiuxiang.react.amap3d
 
 import com.amap.api.maps.AMapUtils
 import com.amap.api.maps.model.LatLng
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.*
 
 @Suppress("unused")
 class AMapUtilsModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
@@ -17,4 +14,12 @@ class AMapUtilsModule(reactContext: ReactApplicationContext) : ReactContextBaseJ
     fun distance(lat1: Double, lng1: Double, lat2: Double, lng2: Double, promise: Promise) {
         promise.resolve(AMapUtils.calculateLineDistance(LatLng(lat1, lng1), LatLng(lat2, lng2)))
     }
+
+    /** add by david  at 2019-05-08 start */
+    @ReactMethod
+    fun getCoordinate(data: ReadableArray, promise: Promise) {
+        promise.resolve(MapUtil.calculateCoordinate(data,currentActivity))
+    }
+    /** add by david  at 2019-05-08 end */
+
 }
--- ios/RNAMap/AMapUtils.m
+++ ios/RNAMap/AMapUtils.m
@@ -1,6 +1,6 @@
 #import <React/RCTBridgeModule.h>
 #import <MAMapKit/MAGeometry.h>
-
+#import <AMapFoundationKit/AMapFoundationKit.h>
 #pragma ide diagnostic ignored "OCUnusedClassInspection"
 
 @interface AMapUtils : NSObject <RCTBridgeModule>
@@ -21,5 +21,40 @@ RCT_EXPORT_METHOD(distance:(double)lat1
             MAMapPointForCoordinate(CLLocationCoordinate2DMake(lat2, lng2))
     )));
 }
+
+
+/** add by david  2019-05-08 start */
+//gps坐标转换成高德坐标
+RCT_EXPORT_METHOD(getCoordinate:
+                  (NSArray *)array
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject ) {
+    
+    @try {
+        NSInteger count =array.count;
+        NSMutableArray *mutableArray = [NSMutableArray arrayWithCapacity:count];
+        
+        for (NSInteger i=0; i<count; i++) {
+            NSMutableDictionary *dic = array[i];
+            NSString *lat = dic[@"lat"];
+            NSString *log = dic[@"long"];
+            CLLocationDegrees latitude = [lat doubleValue];
+            CLLocationDegrees longitude = [log doubleValue];
+            CLLocationCoordinate2D amapcoord = AMapCoordinateConvert(CLLocationCoordinate2DMake(latitude,longitude), AMapCoordinateTypeGPS);
+            
+            //            NSLog(@"amapcoord=%@", amapcoord);
+            //            [dic setObject:latitude  forKey:@"lat"];
+            NSNumber *latNum = [NSNumber numberWithDouble:amapcoord.latitude];
+            NSNumber *longNum = [NSNumber numberWithDouble:amapcoord.longitude];
+            [dic setObject:latNum forKey:@"lat"];
+            [dic setObject:longNum forKey:@"long"];
+            [mutableArray addObject:dic];
+        }
+        resolve(mutableArray);
+    } @catch (NSException *exception) {
+        
+    }
+}
+/** add by david  2019-05-08 end */
 
 @end
